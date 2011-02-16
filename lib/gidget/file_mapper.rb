7b7659e8ba5e4268d8b2d8507ca4234d1ec3b92a
hasmanytrees <forrest@hasmanytrees.com>
2011-02-15 21:26:14 -0700

    cleanup

--- lib/gidget/file_mapper.rb
+++ lib/gidget/file_mapper.rb
@@ -14,34 +14,31 @@ module Gidget
       @pages = Hash.new
       @posts = Array.new
       
-      puts Benchmark.measure { self.load }
-    end
+      puts Benchmark.measure {
+        page_paths = Dir.glob("pages/**/*.txt")
 
-    
-    def load
-      page_paths = Dir.glob("pages/**/*.txt")
+        # load the page index
+        page_paths.each { |file_path|
+          page = Page.new(file_path)
+          @pages[page.request_path] = page
+        }
 
-      # load the page index
-      page_paths.each { |file_path|
-        page = Page.new(file_path)
-        @pages[page.request_path] = page
+        puts "Page Index created, size = " + @pages.keys.size.to_s
+
+
+        post_paths = Dir.glob("posts/**/*.txt")
+
+        # load the post index
+        post_paths.each { |file_path|
+          post = Post.new(file_path)
+          @posts << post
+        }
+
+        # sort the post index by date descending (newest will be first)
+        @posts.replace @posts.sort_by { |p| Time.parse(p.date.to_s).to_i }.reverse!
+
+        puts "Post Index created, size = " + @posts.size.to_s
       }
-      
-      puts "Page Index created, size = " + @pages.keys.size.to_s
-      
-      
-      post_paths = Dir.glob("posts/**/*.txt")
-
-      # load the post index
-      post_paths.each { |file_path|
-        post = Post.new(file_path)
-        @posts << post
-      }
-      
-      # sort the post index by date descending (newest will be first)
-      @posts.replace @posts.sort_by { |p| Time.parse(p.date.to_s).to_i }.reverse!
-      
-      puts "Post Index created, size = " + @posts.size.to_s
     end
     
     
